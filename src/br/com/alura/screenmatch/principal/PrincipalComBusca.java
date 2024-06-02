8d67e1a0b9854af224e1c56e669e436989762cf2
GabrielRochaFC <gabriel.rocha20@live.com>
2024-06-02 16:05:32 -0300

    Creating json files

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,50 +7,75 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        System.out.println("Digite o filme que deseja buscar: ");
         Scanner sc = new Scanner(System.in);
-        String filme = sc.nextLine();
-        String buscaFilme = "https://www.omdbapi.com/?t=" + filme + "&apikey=eddea30a";
-        try{
-            // Primeiramente temos que ter um cliente
-            HttpClient client = HttpClient.newHttpClient();
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+        List<Titulo> filmes = new ArrayList<>();
+        String filme = "";
 
-            // Depois fazemos a requisição
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(buscaFilme))
-                    .build();
-
-            // Agora precisamos pegar a resposta, e deve tratar uma exception
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        while (!filme.equalsIgnoreCase("sair")) {
 
 
-            String json = response.body();
+            System.out.println("Digite o filme que deseja buscar: ");
+            filme = sc.nextLine();
 
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
+            if (filme.equalsIgnoreCase("sair")){
+                break;
+            }
 
-            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            // Para funcionar bote a sua APIkey aqui:
+            String buscaFilme = "https://www.omdbapi.com/?t=" + filme + "&apikey=";
 
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println("Título já convertido");
-            System.out.println(meuTitulo);
-        } catch (NumberFormatException e) {
-            System.out.println("Aconteceu um erro: ");
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Algum erro de argumento na busca, verifique o endereço");
-        } catch (ErroDeConversaoDeAnoException e) {
-            System.out.println(e.getMessage());
+
+            try {
+                // Primeiramente temos que ter um cliente
+                HttpClient client = HttpClient.newHttpClient();
+
+                // Depois fazemos a requisição
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(buscaFilme))
+                        .build();
+
+                // Agora precisamos pegar a resposta, e deve tratar uma exception
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+
+                String json = response.body();
+
+
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println("Título já convertido");
+                System.out.println(meuTitulo);
+
+                filmes.add(meuTitulo);
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereço");
+            } catch (ErroDeConversaoDeAnoException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(filmes));
+        escrita.close();
         System.out.println("O programa finalizou corretamente!");
 
     }
